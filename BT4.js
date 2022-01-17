dc2c5b769d4fa16ae9d38cede4d9ae977b8fd3bb
unknown <nqphuong010j1@gmail.com>
2022-01-17 11:36:13 +0700

    BTJS4

---
+++ BT4.js
@@ -0,0 +1,33 @@
+let name = prompt("Nhập tên");
+let place = prompt('Nhập địa điểm');
+let time = prompt('Nhập thời gian(dd/mm/yy)');
+
+document.write('"'+ name + ' '+'thương nhớ,');
+document.write('<br>');
+document.write('<br>');
+document.write('Em không biết phải nói sao để anh hiểu rằng, em nhớ anh thật nhiều. ');
+document.write('<br>');
+document.write('Em yêu anh đến khi trái tim này tan thành nghìn mảnh.');
+document.write('<br>');
+document.write('Tất cả những gì em yêu thương, em khát khao và cần đến, chính là anh, mãi mãi về sau. ');
+document.write('<br>');
+document.write('Em chỉ muốn ở bên anh, và anh yêu hỡi, em sẽ trở thành người phụ nữ như anh mong muốn.');
+document.write('<br>');
+document.write('<br>');
+document.write('Có phải em thật tệ hại, khi cứ nghĩ đến anh thật nhiều, thật lâu và nhất là mỗi khi đêm xuống?');
+document.write('<br>');
+document.write('Em hứa sẽ sẽ cố gắng triệu triệu lần hơn thế. ');
+document.write('<br>');
+document.write('Nhưng hơn tất cả, em chỉ mong một ngày nào đó, anh sẽ tự hào về em, như tự hào về vợ của anh, và mẹ của các con anh (ít nhất là 2 nhé, em vừa mới quyết định đấy!). ');
+document.write('<br>');
+document.write('Em nhớ thật nhiều cảm giác mỗi đêm anh ôm em và ru em ngủ trong vòng tay. ');
+document.write('<br>');
+document.write('Đêm nay, em chỉ muốn được gần bên anh… và anh biết không, trái tim em đang đau đớn đến nhường nào.');
+document.write('<br>');
+document.write('<br>');
+document.write('Anh yêu thương, đừng bao giờ rời xa em nữa nhé. Yêu anh rất nhiều.');
+document.write('<br>');
+document.write('<br>');
+document.write(place + ',' +time+'.'+'"');
+
+
